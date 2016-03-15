473a3b0463a00ae890ea24314cb20bd0aaa41865
Malte Berneaud <malte.berneaud@gmail.com>
2016-03-15 08:37:16 +0100

    Did first graph

--- Analysis/Modeling/graphs.R
+++ Analysis/Modeling/graphs.R
@@ -7,4 +7,17 @@
 
 # loading libraries
 library(ggplot2)
+library(dplyr)
 
+# reading data
+CYmerge <- read.csv("Analysis/Merge/CYmerge.csv")
+
+# creating national averages
+CYmerge <- group_by(CYmerge, year)
+
+year_averages <- summarise(CYmerge, year.avg = mean(avg.EUS))
+
+# plotting averages
+ggplot(year_averages, aes(x = year, y = year.avg)) + geom_point()
+
+qplot(year_averages$year, year_averages$year.avg, geom =c("point", "line"))
