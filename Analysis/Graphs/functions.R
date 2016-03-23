db5bed45a92ef5cf6ed227e23773679b172ae087
Malte Berneaud <malte.berneaud@gmail.com>
2016-03-23 21:08:39 +0100

    Created new graphs, stuck at grid

---
+++ Analysis/Graphs/functions.R
@@ -0,0 +1,73 @@
+############################
+# Eurosceptic Misfit Master Thesis
+# Author: Malte Berneaud-Kötz
+# Date created: 23.03.16
+# Last edited: 23.03.16
+# Contains functions used for automating plot generation
+
+
+# Function for drawing ggplot with country ID and country names
+drawgg <- function(cid ,cname) {
+  tmp <- dplyr::filter(CYmerge, nation == cid)
+  tmp <- select(tmp, nation, year, sum.EUS.vote, avg.EUS, diff.EUS)
+  g <- ggplot(tmp, aes(x = year)) +
+    geom_line(aes(y = avg.EUS, colour = "avg.EUS")) +
+    geom_line(aes(y = sum.EUS.vote, colour = "sum.EUS.vote")) +
+    geom_line(aes(y = diff.EUS, colour = "diff.EUS")) +
+    scale_colour_discrete(name = "Euroscepticism variables", 
+                          labels = c("Eurosceptic misfit", 
+                                     "Popular Euroscepticism",
+                                     "Eurosceptic vote share")) +
+    ylab("Variable value in %") + xlab("Year of EP election") + 
+    theme(legend.position = "top") + ggtitle(cname)
+  class(g) <- "ggplot"
+  print(g)
+}
+
+
+# Multiple plot function credit goes to Winston Chang 
+# (http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_%28ggplot2%29/)
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
--- Analysis/Graphs/graphs.R
+++ Analysis/Graphs/graphs.R
@@ -9,6 +9,12 @@
 library(ggplot2)
 library(dplyr)
 library(car)
+library(grid)
+library(gridExtra)
+library(cowplot)
+
+# Setting ggplot font size for entire session
+
 
 ####################################
 # IV and DV over time
@@ -29,25 +35,28 @@ graph$year[graph$year == 1995 | graph$year == 1996] <- 1994
 # creating national averages
 graph <- group_by(graph, year)
 
-year_averages <- summarise(graph, gen.avg = mean(gen.EUS, na.rm = TRUE),
-                           inst.avg = mean(inst.EUS, na.rm = TRUE), 
+year_averages <- summarise(graph, EUS.avg = mean(avg.EUS, na.rm = TRUE),
+                           vote.avg = mean(sum.EUS.vote, na.rm = TRUE), 
                            diff.avg = mean(diff.EUS, na.rm = TRUE))
 
 
 # plotting averages for all countries
 averages <- ggplot(year_averages, aes(x = year)) +
-              geom_line(aes(y = inst.avg, colour = "inst.avg")) +
-              geom_line(aes(y = gen.avg, colour = "gen.avg")) +
+              geom_line(aes(y = EUS.avg, colour = "EUS.avg")) +
+              geom_line(aes(y = vote.avg, colour = "vote.avg")) +
               geom_line(aes(y = diff.avg, colour = "diff.avg"))
 
-averages + scale_colour_discrete(name = "Variables measuring Euroscepticism",
-                              labels = c("Eurosceptic Misfit", 
-                                         "average general Euroscepticism",
-                                         "average instrumental Euroscepticism"))+
-  ylab("Variable value in %") + xlab("Year of EP election")
+averages + scale_colour_discrete(name = "Euroscepticism variables", 
+                                 labels = c("Eurosceptic misfit", 
+                                            "Popular Euroscepticism",
+                                            "Eurosceptic vote share")) +
+  ylab("Variable value in %") + xlab("Year of EP election") + 
+  theme(legend.position = "top") + theme(text = element_text(size = 12))
 
-ggsave("Analysis/Graphs/DP_and_IVs_over_time.png")
-ggsave("Analysis/Graphs/DP_and_IVs_over_time.pdf")
+ggsave("Analysis/Graphs/DP_and_IVs_over_time.png", width = 210, height = 120,
+       units = "mm")
+ggsave("Analysis/Graphs/DP_and_IVs_over_time.pdf", width = 210, height = 120,
+       units = "mm")
 
 
 
@@ -97,8 +106,44 @@ order <- reorder(a$c.names, a$x)
 ggplot(a, aes(x=order, y=x, group=member.f, colour=member.f)) +
   geom_point() +
   ylab("Country-specific errors") + xlab("Member State") +
-  scale_colour_discrete(name = "Year of accession")
+  scale_colour_discrete(name = "Year of accession") +
+  theme(text = element_text(size = 12))
 
 # saving the plot
-ggsave("Analysis/Graphs/country-specific_error.pdf")
+ggsave("Analysis/Graphs/country-specific_error.pdf", width = 297, height = 120,
+       units = "mm")
 
+################
+# Eurosceptic misfits in Original 6 MS over time
+################
+source("Analysis/Graphs/functions.R")
+
+# creating data frame with id and country names
+ids <- data.frame(nation = c(21, 22, 23, 31, 32, 41), 
+                  name = c("Belgium", "The Netherlands", "Luxembourg", "France",
+                           "Italy", "Germany"))
+
+# Creating list will all plots inside it 
+plots <- list()
+for(i in 1:nrow(ids)) {
+  plot <- drawgg(ids$nation[i], ids$name[i])
+  plots[[i]] <- plot
+}
+
+
+
+# Drawing graphs by hand
+DE <- drawgg(41, "Germany")
+class(DE) <- c("gg", "ggplot")
+BE <- drawgg(21, "Belgium")
+class(BE) <- c("gg", "ggplot")
+NL <- drawgg(22, "The Netherlands")
+class(NL) <- c("gg", "ggplot")
+LU <- drawgg(23, "Luxembourg")
+class(LU) <- c("gg", "ggplot")
+FR <- drawgg(31, "France")
+class(FR) <- c("gg", "ggplot")
+IT <- drawgg(32, "Italy")
+class(IT) <- c("gg", "ggplot")
+
+plot_grid(DE, BE, NL, LU, FR, IT, ncol = 2, nrow = 3)
